65ab8992a24f4605af784a67165b2647494c9029
Lindsey Lawson <lynseahoss@gmail.com>
2020-06-30 21:02:47 -0400

    added table.jsx & TableBody.jsx

---
+++ src/components/TableBody.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ const TableBody = props=>{
+    return(
+        <tr>
+        <th scope="col">{props.index + 1}</th>
+        <th scope="col">{`${props.User.name.first} ${props.User.name.last}`}</th>
+        <th scope="col">{props.User.email}</th>
+        <th scope="col">{props.User.cell}</th>
+      </tr>
+    )
+}
+
+export default TableBody
---
+++ src/components/table.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import User from "../lib/user.json";
+import TableBody from "./TableBody";
+export default class Table extends Component {
+  state = {
+    mainList: [],
+    activeList: []
+  };
+  componentDidMount() {
+    this.setState({
+      mainList: User,
+      activeList: User
+    });
+  }
+  searchHandler = term => {
+    const newList = this.state.mainList.filter(
+      user =>
+        user.name.first.toLowerCase().includes(term.toLowerCase()) ||
+        user.name.last.toLowerCase().includes(term.toLowerCase())
+       );
+      this.setState({
+          activeList: newList
+      })
+  };
+  render() {
+    //return is a callback
+    return (
+      <>
+        <input onChange={event => this.searchHandler(event.target.value)} />
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Cell Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* that is a component below */}
+            {this.state.activeList.map((user, index) => (
+              <TableBody User={user} index={index} key={user.cell} />
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
